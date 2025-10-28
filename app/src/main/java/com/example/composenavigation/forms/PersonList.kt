159e74cd990d6f285e62371f4d10249a249ca573
artur <artur.arciuch@gmail.com>
2025-10-28 13:04:26 +0100

    dodawanie parametrów z route

--- app/src/main/java/com/example/composenavigation/forms/PersonList.kt
+++ app/src/main/java/com/example/composenavigation/forms/PersonList.kt
@@ -1,18 +1,14 @@
-import android.R.attr.name
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,13 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.Home
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.composenavigation.Persons
-import kotlin.random.Random
 
 
 @Composable
@@ -44,7 +36,7 @@ fun PersonList(navController: NavController, persons: MutableList<Persons>, modi
 }
 
 @Composable
-fun Zawartosc(navController: NavController, persons: MutableList<Persons>, modifier: Modifier = Modifier) {
+fun Zawartosc(navController: NavController, persons: MutableList<Persons>, modifier: Modifier) {
     var index by remember { mutableStateOf(0) }
     LazyColumn(modifier = Modifier
         .fillMaxWidth()
